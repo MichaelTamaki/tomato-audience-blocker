79f98123c6c6e981f9c64c3de4041d72583f6bf3
Michael Tamaki <themichaeltamaki@gmail.com>
2023-02-23 12:19:03 -1000

    V1.1 Handle TV scores

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,25 @@
 const IRRELEVANT = 'Irrelevant';
-try {
-  // Assume that the score audience percentage is available right away when the script is loaded
-  const scorePanel = document.querySelector('[data-qa=score-panel]');
-  const scoreAudienceIcon = scorePanel.shadowRoot.querySelector('[data-qa=score-icon-audience]');
-  const scoreAudience = scoreAudienceIcon.shadowRoot.querySelector('[data-qa=audience-score');
-  scoreAudience.innerText = IRRELEVANT;
 
-  // If the score audience percentage changes later, then catch it (i dont think this will happen but just in case)
-  const scoreAudienceObserver = new MutationObserver((mutations, observer) => {
-    mutations.forEach((mutation) => {
-      if (mutation.target.innerText !== IRRELEVANT) {
-        mutation.target.innerText = IRRELEVANT;
-      }
-    });
-  });
-  scoreAudienceObserver.observe(scoreAudience, { characterData: true });
+// Assume that the score audience percentage is available right away when the script is loaded
+try {
+  if (document.querySelector("[data-qa=audience-score]")) {
+    /**
+     * TV
+     * 
+     * The audience score is not in a shadow element
+     */
+    document.querySelector("[data-qa=audience-score]").innerText = IRRELEVANT;
+  } else {
+    /**
+     * MOVIES
+     * 
+     * The audience score is in 2 layers of shadow elements
+     */
+    const scorePanel = document.querySelector('[data-qa=score-panel]');
+    const scoreAudienceIcon = scorePanel.shadowRoot.querySelector('[data-qa=score-icon-audience]');
+    const scoreAudience = scoreAudienceIcon.shadowRoot.querySelector('[data-qa=audience-score');
+    scoreAudience.innerText = IRRELEVANT;
+  }
 } catch (e) {
   console.error(e);
 }
